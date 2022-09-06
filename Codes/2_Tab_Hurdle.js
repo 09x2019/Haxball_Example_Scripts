769c44559c58c53743e214849f4494bf547cddc9
09x2019 <109242174+09x2019@users.noreply.github.com>
2022-09-06 21:33:53 +0300

    Create 2_Tab_Hurdle.js

---
+++ Codes/2_Tab_Hurdle.js
@@ -0,0 +1,19 @@
+var room = HBInit({
+	      roomName: "My room",
+	      maxPlayers: 16,
+        public: false,
+	      noPlayer: true // Remove host player (recommended!)
+});
+
+var PlayerAuth = {};
+
+room.onPlayerJoin = function(player) {
+
+for (var i in PlayerAuth) {
+	if(PlayerAuth[i].auth == player.auth)
+		room.kickPlayer(PlayerAuth[i].id, "Tab detection is blocked. Don't! Your tab in the room: " + player.name, false);
+}
+
+PlayerAuth[player.auth] = player;
+
+}
