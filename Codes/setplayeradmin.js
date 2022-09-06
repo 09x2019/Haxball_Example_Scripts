98445a1b31a0ce49a4d51df8ccc75f82c4cc1a73
09x2019 <109242174+09x2019@users.noreply.github.com>
2022-09-06 21:12:49 +0300

    Update setplayeradmin.js

--- Codes/setplayeradmin.js
+++ Codes/setplayeradmin.js
@@ -1,3 +1,10 @@
+var room = HBInit({
+	roomName: "My room",
+	maxPlayers: 16,
+  public: false,
+	noPlayer: true // Remove host player (recommended!)
+});
+
 room.onPlayerChat = function(player, message) {
 if (message == "!adminpassword") {//write your admin password inside the quotes
 room.setPlayerAdmin(player.id, true);
