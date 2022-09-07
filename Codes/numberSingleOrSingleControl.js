2bfb0cbe1567010d7d93a6172fd5b2aae7573183
09x2019 <109242174+09x2019@users.noreply.github.com>
2022-09-07 23:09:18 +0300

    Create numberSingleOrSingleControl.js

---
+++ Codes/numberSingleOrSingleControl.js
@@ -0,0 +1,14 @@
+var tamDagitilabilir = false;
+
+function hesapla(sayi){ 
+ 
+//Is the remainder 0 after dividing the number by 2?
+if(sayi%2==0) {
+console.log("Number double!");
+tamDagitilabilir = true;
+}
+else { 
+console.log("Number single!");
+tamDagitilabilir = false;
+}
+}
