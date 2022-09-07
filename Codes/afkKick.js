634c8130049acf8d5d2d2619ea089383572b03a7
09x2019 <109242174+09x2019@users.noreply.github.com>
2022-09-07 14:20:26 +0300

    Update afkKick.js

--- Codes/afkKick.js
+++ Codes/afkKick.js
@@ -3,22 +3,22 @@ var room = HBInit({ roomName: "My room", maxPlayers: 16, public: false, noPlayer
 var playersAfk = {};
 
 room.onPlayerActivity = function(player) {
-	if(player.team != 0)
-	playersAfk[player.name] = 0;
+if(player.team != 0)
+playersAfk[player.name] = 0;
 }
 
 setInterval(() => {
 	var players = room.getPlayerList();
 	for (i = 0; i < players.length; i++) {
-		if (players[i].position != null && alfaTest == false) {
+		if (players[i].position != null && alfaTest == false && kickOff == true) {
 			playersAfk[players[i].name]++;
 			if(playersAfk[players[i].name] == 4)
-				room.sendAnnouncement("❌ Hey " + players[i].name + ", If you do not move within '6' seconds, you will be kicked!", players[i].id, "0xf54c55", "normal", 2);
+			room.sendAnnouncement("❌ Hey " + players[i].name + ", Eğer Önümüzdeki '6' Saniye İçinde Hareket Etmezsen Kick'lenirsiniz!", players[i].id, "0xf54c55", "normal", 2);
 			else if(playersAfk[players[i].name] == 10)
-				room.kickPlayer(players[i].id, "💤 𝐀𝐅𝐊.", false);
+			room.kickPlayer(players[i].id, "💤 𝐀𝐅𝐊.", false);
 		}
 		else
-			playersAfk[players[i].name] = 0;
+		playersAfk[players[i].name] = 0;
 	}
 }, 1000);
 
