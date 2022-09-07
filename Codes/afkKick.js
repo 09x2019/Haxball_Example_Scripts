b7959a0b7d5948dfb4228b56fe66cbd6f37aa041
09x2019 <109242174+09x2019@users.noreply.github.com>
2022-09-07 14:18:26 +0300

    Create afkKick.js

---
+++ Codes/afkKick.js
@@ -0,0 +1,25 @@
+var room = HBInit({ roomName: "My room", maxPlayers: 16, public: false, noPlayer: true });
+
+var playersAfk = {};
+
+room.onPlayerActivity = function(player) {
+	if(player.team != 0)
+	playersAfk[player.name] = 0;
+}
+
+setInterval(() => {
+	var players = room.getPlayerList();
+	for (i = 0; i < players.length; i++) {
+		if (players[i].position != null && alfaTest == false) {
+			playersAfk[players[i].name]++;
+			if(playersAfk[players[i].name] == 4)
+				room.sendAnnouncement("❌ Hey " + players[i].name + ", If you do not move within '6' seconds, you will be kicked!", players[i].id, "0xf54c55", "normal", 2);
+			else if(playersAfk[players[i].name] == 10)
+				room.kickPlayer(players[i].id, "💤 𝐀𝐅𝐊.", false);
+		}
+		else
+			playersAfk[players[i].name] = 0;
+	}
+}, 1000);
+
+var alfaTest = false;
