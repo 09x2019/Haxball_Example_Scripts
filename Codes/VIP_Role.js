5ea3e1d24040e6706b8617678478de713415093d
09x2019 <109242174+09x2019@users.noreply.github.com>
2022-09-08 20:33:15 +0300

    Update VIP_Role.js

--- Codes/VIP_Role.js
+++ Codes/VIP_Role.js
@@ -1,4 +1,4 @@
-var room = HBInit({ roomName: "My room", maxPlayers: 16, public: false, noPlayer: true });
+var room = HBInit({ roomName: "VIP Role", maxPlayers: 16, public: false, noPlayer: true });
 
 //style[number] 0 normal - 1 bold - 2 italic - 3 small - 4 small-bold - 5 small-italic
 //sounds[number] 0 - 1 - 2
